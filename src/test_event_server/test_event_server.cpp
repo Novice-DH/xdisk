108ce822d85fc1587e9ca456bea2176999133e13
Galaxy-DH <huangdcn@163.com>
2023-01-05 21:05:04 +0800

    change test event server on Linux

--- src/test_event_server/test_event_server.cpp
+++ src/test_event_server/test_event_server.cpp
@@ -1,28 +1,32 @@
-﻿//#define _CRT_SECURE_NO_WARNINGS
+﻿// #define _CRT_SECURE_NO_WARNINGS
 #include <event2/event.h>
 #include <iostream>
 
 #ifdef _WIN32
 #else
 #include <signal.h>
+#include <cstring>
 #endif
 using namespace std;
 
-void ListenCB(evutil_socket_t sock, short what, void* arg) {
+void ListenCB(evutil_socket_t sock, short what, void *arg)
+{
     cout << "ListenCB" << endl;
-    if (!(what & EV_READ)) {
+    if (!(what & EV_READ))
+    {
         cout << "not read" << endl;
         return;
     }
     sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     socklen_t size = sizeof(sin);
-    evutil_socket_t client_socket = accept(sock, (sockaddr*)&sin, &size);
-    if (client_socket <= 0) {
+    evutil_socket_t client_socket = accept(sock, (sockaddr *)&sin, &size);
+    if (client_socket <= 0)
+    {
         cerr << "accept error!" << endl;
         return;
     }
-    char ip[16] = { 0 };
+    char ip[16] = {0};
     evutil_inet_ntop(AF_INET, &sin.sin_addr, ip, sizeof(ip));
     cout << "client ip is " << ip << endl;
 }
@@ -30,32 +34,36 @@ void ListenCB(evutil_socket_t sock, short what, void* arg) {
 int main(int argc, char *argv[])
 {
     int server_port = 20010;
-    if (argc > 1) {
+    if (argc > 1)
+    {
         server_port = atoi(argv[1]);
     }
 #ifdef _WIN32
-    //初始化 socket 库
+    // 初始化 socket 库
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
 #else
     // Linux 使用断开连接的 socket
     // 会发出此信号，造成程序退出
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+    {
         return 1;
     }
 #endif
-     
+
     std::cout << "test event server!\n";
     // 1 创建 libevent 的上下文
     // 默认创建 base 锁，是线程安全的
-    event_base * base = event_base_new();
-    if (base) {
+    event_base *base = event_base_new();
+    if (base)
+    {
         cout << "event_base_new success!" << endl;
     }
 
     // 2 创建 socket，绑定 port
     evutil_socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock <= 0) {
+    if (sock <= 0)
+    {
         // _CRT_SECURE_NO_WARNINGS
         cerr << "socket error" << strerror(errno) << endl;
         return -1;
@@ -66,16 +74,17 @@ int main(int argc, char *argv[])
     evutil_make_listen_socket_reuseable(sock);
 
     // 绑定端口
-    //sockaddr sin; // err
+    // sockaddr sin; // err
     sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_port = htons(server_port);
-    //sin.sin_addr.s_addr = htonl(0);
-    int re = ::bind(sock, (sockaddr*)&sin, sizeof(sin));
-    if (re != 0) {
+    // sin.sin_addr.s_addr = htonl(0);
+    int re = ::bind(sock, (sockaddr *)&sin, sizeof(sin));
+    if (re != 0)
+    {
         // _CRT_SECURE_NO_WARNINGS
-        cerr << "bind port:" << server_port <<" error!" << strerror(errno) << endl;
+        cerr << "bind port:" << server_port << " error!" << strerror(errno) << endl;
         return -1;
     }
     listen(sock, 10);
