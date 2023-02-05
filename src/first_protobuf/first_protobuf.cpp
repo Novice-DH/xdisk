25e1805e16e1fe707c4daaafc6737d5290ef59ac
Galaxy-DH <huangdcn@163.com>
2023-02-05 22:12:08 +0800

    add first_protobuf, reflect_protobuf and dynamic_reflect_protobuf

---
+++ src/first_protobuf/first_protobuf.cpp
@@ -0,0 +1,54 @@
+﻿#include "xmsg_head.pb.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace xmsg;
+
+int main()
+{
+    cout << "test protobuf!\n";
+    XMsgHead msg1;
+    msg1.set_msg_size(1024);
+    msg1.set_msg_str("test0001");
+
+    cout << "msg1 size = " << msg1.msg_size() << endl;
+    cout << "msg1 msg_str = " << msg1.msg_str() << endl;
+
+    // 序列化到 string
+    cout << "序列化到 string：-----------------" << endl;
+    string str1;
+    msg1.SerializeToString(&str1);
+    cout << "str1 size = " << str1.size() << endl;
+    cout << "str1 = " << str1 << endl;
+
+    // 序列化到 文件
+    cout << "序列化到 文件：-----------------" << endl;
+    ofstream ofs;
+    ofs.open("test.txt", ios::binary); // 应以 二进制 方式写入，而不能 以 默认 ASCII 方式写入
+    msg1.SerializePartialToOstream(&ofs);
+    ofs.close();
+
+    // 从文件 反序列化
+    cout << "从文件 反序列化：-----------------" << endl;
+    ifstream ifs;
+    ifs.open("test.txt", ios::binary);
+    XMsgHead msg2;
+    cout << msg2.ParseFromIstream(&ifs) << endl;
+    cout << "msg2 str = " << msg2.msg_str() << endl;
+
+    // 从 string 中 反序列化
+    cout << "从 string 中 反序列化：-----------------" << endl;
+    msg2.set_msg_str("change msg2 str");
+    string str2;
+    msg2.SerializeToString(&str2);
+    cout << "msg2 size = " << str2.size() << endl;
+    cout << "msg2 str = " << str2 << endl;
+
+    XMsgHead msg3;
+    msg3.ParseFromArray(str2.data(), str2.size());
+    cout << "msg3 size = " << msg3.msg_size() << endl;
+    cout << "msg3 str = " << msg3.msg_str() << endl;
+
+    //system("pause");
+}
---
+++ src/reflect_protobuf/reflect_protobuf.cpp
@@ -0,0 +1,91 @@
+﻿#include "xmsg_head.pb.h"
+#include <iostream>
+#include <fstream>
+#include <google/protobuf/compiler/importer.h>
+#include <google/protobuf/dynamic_message.h>
+
+using namespace std;
+using namespace xmsg;
+using namespace google;
+using namespace protobuf;
+using namespace compiler;
+
+int main()
+{
+    cout << "test reflect protobuf!\n";
+
+    // 通过 message 对象 和 字段名称 设置和获取内容
+    XMsgHead msg;
+
+    // 对象的描述对象
+    auto descriptor = msg.GetDescriptor();
+
+    // 反射对象
+    auto reflector = msg.GetReflection();
+
+    // 属性对象
+    string fname = "msg_size";
+    auto size_field = descriptor->FindFieldByName(fname);
+    if (!size_field)
+    {
+        cerr << "FindFieldByName " << fname << "  failed!" << endl;
+        return -1;
+    }
+
+    // 设置属性的值
+    reflector->SetInt32(&msg, size_field, 1024);
+
+    // 获取属性的值
+    cout << fname << " = " << reflector->GetInt32(msg, size_field) << endl;
+
+    ////////////////////////////////
+    //// 运行时解析 proto 文件
+    // 准备文件系统
+    DiskSourceTree source_tree;
+    // 设置一个字符串，用来作路径替换
+    source_tree.MapPath("protobuf_root", "./");
+
+    // 创建动态编译器
+    Importer importer(&source_tree, NULL);
+    string protobuf_filename = "xmsg_head.proto";
+    // 动态编译 proto 文件，若已编译过，直接返回缓冲对象
+    string path = "protobuf_root/";
+    path += protobuf_filename;
+    // 返回文件的描述对象，包含文件依赖关系
+    auto file_desc = importer.Import(path);
+    if (!file_desc)
+    {
+        cerr << "importer.Import " << path << " failed!" << endl;
+        return -1;
+    }
+    // Message 类型
+    auto message_desc = importer.pool()->FindMessageTypeByName("xmsg.XMsgHead");
+
+    // 用消息工厂，创建消息对象
+    DynamicMessageFactory factory;
+    // 创建一个类型原型
+    auto message_proto = factory.GetPrototype(message_desc);
+    auto msg_test = message_proto->New();
+    { // 使得变量名不冲突，每个栈空间的作用域为 {}
+        // 对象的描述对象
+        auto descriptor = msg_test->GetDescriptor();
+
+        // 反射对象
+        auto reflector = msg_test->GetReflection();
+
+        // 属性对象
+        string fname = "msg_str";
+        auto str_field = descriptor->FindFieldByName(fname);
+        if (!str_field)
+        {
+            cerr << "FindFieldByName " << fname << "  failed!" << endl;
+            return -1;
+        }
+
+        // 设置属性的值
+        reflector->SetString(msg_test, str_field, "test dy_proto str");
+
+        // 获取属性的值
+        cout << "test dy_proto " << fname << " = " << reflector->GetString(*msg_test, str_field) << endl;
+    }
+}
